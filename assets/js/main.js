2b8feb8ad6a2d3a5d205793e0d7cc77a079ae625
bobbyfromgeminus <info.geminus@gmail.com>
2023-08-31 15:33:38 +0200

    first state

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+const slider = document.querySelector('.slider-container');
+const slides = document.querySelectorAll('.slider-container article');
+const interval = 3000;
+
+let curr = 0;
+let prev = 0;
+
+function showSlide() {
+  if (curr == 0 || curr == slides.length) {
+    curr = 0;
+    prev = slides.length-1;
+  } else {
+    prev = curr-1;
+  }
+
+  slides[prev].classList.remove('active'); // Előző kép inaktív
+  slides[curr].classList.add('active'); // Új kép aktív
+
+  console.log(curr, prev);
+
+  curr++;
+
+}
+
+showSlide();
+setInterval(showSlide, interval);
